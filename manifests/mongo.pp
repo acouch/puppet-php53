b7ffb36deb61581843f44305279a746f26dce898
acouch <acinternets@gmail.com>
2013-02-22 14:50:31 -0600

    Update to mongo

--- manifests/mongo.pp
+++ manifests/mongo.pp
@@ -2,14 +2,14 @@ class php53::mongo inherits php53 {
 
   package { 'mongo':
     name => [
-      'mongo',
+      'mongodb',
     ],
       ensure => installed
   }
 
   exec { 'pecl-install-mongo':
     command => "/usr/bin/pecl install mongo",
-    unless => "/usr/bin/pear remote-list -c mongo",
+    unless => "/usr/bin/pecl info mongo",
     require => Package["mongo"],
   }
 
